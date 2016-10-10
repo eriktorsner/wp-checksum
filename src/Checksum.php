2cc38eea674a83cca60f82944c20646146ca7c8d
Erik Torsner <erik@torgesta.com>
2016-10-11 01:15:22 +0200

    refactoring

--- src/Checksum.php
+++ src/Checksum.php
@@ -1,6 +1,8 @@
 <?php
 namespace WPChecksum;
 
+use Pimple\Container;
+
 class Checksum
 {
     /**
@@ -10,12 +12,24 @@ class Checksum
     private static $application;
 
     /**
+     * Internal log object
+     * @var object
+     */
+    private $logger;
+
+    /**
+     * Internal object to retreive settings
+     * @var object
+     */
+    private $settings;
+
+    /**
      * @return \Pimple\Container
      */
     public static function getApplication()
     {
         if (!self::$application) {
-            self::$application = new \Pimple\Container();
+            self::$application = new Container();
             self::$application->register(new \ParametersProvider());
         }
 
@@ -36,7 +50,9 @@ class Checksum
      * ## OPTIONS
      *
      * [<type>]
-     * : What should we do checksums on? plugin|theme. Omit to check everything
+     * : What should we do report on? plugin|theme|quota. Omit to check everything
+     * use quota to see API rate limits for current api key
+     *
      * [<slug>]
      * : Name of a specific plugin or theme to check. Leave blank to check all installed
      *
@@ -62,40 +78,70 @@ class Checksum
     public function __invoke($args, $assocArgs)
     {
         $app = self::getApplication();
-        list($type, $details, $localCache, $format) = $this->parseArgs($args, $assocArgs);
+        $this->logger = $app['logger'];
+        $this->settings = $app['settingsParser'];
+
+        $type = 'all';
+        if (count($args) > 0) {
+            $type = $args[0];
+            if (!in_array($type, array('plugin', 'theme', 'quota'))) {
+                $this->logger->logError('Invalid type specified. Use one of plugin|theme or quota');
+            }
+        }
+
+        $details = $this->settings->getSetting('details', 'boolean', false);
+        $format = $this->settings->getSetting('format', 'string', 'table');
+        if (!in_array($format, array('table', 'json', 'csv', 'yaml'))) {
+            $this->logger->logError('Invalid format specified. Use one of table|json|csv|yaml');
+        }
 
         switch ($type) {
             case 'all':
-                $plugins = $this->checkPlugins($args, $localCache);
-                $themes = $this->checkThemes($args, $localCache);
+                $plugins = $this->checkPlugins($args);
+                $themes = $this->checkThemes($args);
                 $out = array_merge($plugins, $themes);
                 break;
             case 'plugin':
                 array_shift($args);
-                $out = $this->checkPlugins($args, $localCache);
+                $out = $this->checkPlugins($args);
                 break;
             case 'theme':
                 array_shift($args);
-                $out = $this->checkThemes($args, $localCache);
+                $out = $this->checkThemes($args);
                 break;
-            default:
-                \WP_CLI::error('Invalid type specified. Use one of plugin|theme|core');
+            case 'quota':
+                $apiClient = new ApiClient();
+                $out = $apiClient->getQuota();
+                if ($out) {
+                    $ret = array((array)$out);
+                    $this->logger->formatItems($format, $ret, 'limit, current, resetIn');
+                } else {
+
+                }
+
+                return;
                 break;
         }
 
-        if ($details) {
-            $this->outputDetailedResults($out, $format);
-        } else {
-            $this->outputSummaryResults($out, $format);
-        }
+        $this->outputResults($out, $format, $details);
     }
 
+    /**
+     * Run checksums for all plugins or a single named plugin
+     *
+     * @param $args
+     * @param bool $localCache
+     *
+     * @return array
+     */
     private function checkPlugins($args, $localCache = false)
     {
         $scope = 'all';
+
         if (count($args) > 0) {
             $scope = $args[0];
         }
+
         $plugins = get_plugins();
         $out = array();
         foreach ($plugins as $id => $plugin) {
@@ -104,7 +150,8 @@ class Checksum
             if ($scope != 'all' && $scope != $slug) {
                 continue;
             }
-            \WP_CLI::log("Checking plugin $slug");
+
+            $this->logger->log("Checking plugin $slug");
             $checker = new PluginChecker($localCache);
             $out[] = $checker->check($id, $plugin);
         }
@@ -112,6 +159,14 @@ class Checksum
         return $out;
     }
 
+    /**
+     * Run checksums for all themes or a single named theme
+     *
+     * @param $args
+     * @param bool $localCache
+     *
+     * @return array
+     */
     private function checkThemes($args, $localCache = false)
     {
         $scope = 'all';
@@ -126,10 +181,11 @@ class Checksum
             }
             // Don't attempt to check child themes
             if ($theme->template != $slug) {
-                \WP_CLI::log("Skipping theme $slug (child theme)");
+                $this->logger->log("Skipping theme $slug (child theme)");
                 continue;
             }
-            \WP_CLI::log("Checking theme $slug");
+
+            $this->logger->log("Checking theme $slug");
             $checker = new ThemeChecker($localCache);
             $out[] = $checker->check($slug, $theme);
         }
@@ -137,10 +193,17 @@ class Checksum
         return $out;
     }
 
-    private function outputDetailedResults($data, $format)
+    /**
+     * Output checksum results
+     *
+     * @param array $dataRows  The list of plugins/thems that have been checked
+     * @param string $format   table|csv|json|yaml
+     * @param boolean $details true|false
+     */
+    private function outputResults($dataRows, $format, $details)
     {
         $out = array();
-        foreach ($data as $data) {
+        foreach ($dataRows as $data) {
             $row = array();
             $row['Type'] = $data['type'];
             $row['Slug'] = $data['slug'];
@@ -152,9 +215,13 @@ class Checksum
                     $row['Issues'] = null;
                 } else {
                     $row['Result'] = 'Changes detected';
-                    $row['Issues'] = array();
-                    foreach ($data['changeset'] as $key => $change) {
-                        $row['Issues'][$key] = $change->status;
+                    if ($details) {
+                        $row['Issues'] = array();
+                        foreach ($data['changeset'] as $key => $change) {
+                            $row['Issues'][$key] = $change->status;
+                        }
+                    } else {
+                        $row['Issues'] = count($data['changeset']);
                     }
                 }
             } else {
@@ -164,71 +231,6 @@ class Checksum
             $out[] = $row;
         }
 
-        \WP_CLI\Utils\format_items($format, $out, 'Type, Slug, Status, Version, Result, Issues');
-    }
-
-    private function outputSummaryResults($data, $format)
-    {
-        $out = array();
-        foreach ($data as $data) {
-            $row = array();
-            $row['Type'] = $data['type'];
-            $row['Slug'] = $data['slug'];
-            $row['Version'] = $data['version'];
-            $row['Status'] = ucfirst($data['status']);
-            if ($data['status'] == 'checked') {
-                $row['Result'] = count($data['changeset']) == 0? 'Ok': 'Changes detected';
-                $row['Issues'] = count($data['changeset']);
-            } else {
-                $row['Result'] = null;
-                $row['Issues'] = null;
-            }
-
-            $out[] = $row;
-        }
-
-        \WP_CLI\Utils\format_items($format, $out, 'Type, Slug, Status, Version, Result, Issues');
-    }
-
-    private function parseArgs($args, $assocArgs)
-    {
-        $cli = \WP_CLI::get_runner();
-
-        $type = 'all';
-        if (count($args) > 0) {
-            $type = $args[0];
-            if (!in_array($type, array('plugin', 'theme'))) {
-                \WP_CLI::error('Invalid type specified. Use one of plugin|theme');
-            }
-        }
-
-        $localCache = false;
-        if (isset($cli->extra_config['checksum']['localcache'])) {
-            $localCache = filter_var(
-                $cli->extra_config['checksum']['localcache'], FILTER_VALIDATE_BOOLEAN
-            );
-        }
-        $localCache = \WP_CLI\Utils\get_flag_value($assocArgs, 'localcache', false);
-
-        $details = false;
-        if (isset($cli->extra_config['checksum']['details'])) {
-            $details = filter_var(
-                $cli->extra_config['checksum']['details'], FILTER_VALIDATE_BOOLEAN
-            );
-        }
-        $details = \WP_CLI\Utils\get_flag_value($assocArgs, 'details', false);
-
-        $format = 'table';
-        if (isset($cli->extra_config['checksum']['format'])) {
-            $format = $cli->extra_config['checksum']['format'];
-        }
-        if (isset($assocArgs['format'])) {
-            $format = $assocArgs['format'];
-        }
-        if (!in_array($format, array('table', 'json', 'csv', 'yaml'))) {
-            \WP_CLI::error('Invalid format specified. Use one of table|json|csv|yaml');
-        }
-
-        return array($type, $details, $localCache, $format);
+        $this->logger->formatItems($format, $out, 'Type, Slug, Status, Version, Result, Issues');
     }
 }
