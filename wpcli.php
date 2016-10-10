7ace6aeb1fb8bf281a5254230a435b84fce869f4
Erik Torsner <erik@torgesta.com>
2016-10-11 01:16:35 +0200

    namespace change

--- wpcli.php
+++ wpcli.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/ParametersProvider.php';
-require_once __DIR__ . '/Checksum.php';
 
 if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
     WP_CLI::add_command('checksum', 'WPChecksum\Checksum');
