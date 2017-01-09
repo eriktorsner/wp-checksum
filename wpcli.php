24be0fe2b0289393c08cc0efd3578697d87be18e
Erik Torsner <erik@torgesta.com>
2017-01-09 12:24:23 +0100

    Setting up subcommands wp-cli style

--- wpcli.php
+++ wpcli.php
@@ -2,5 +2,12 @@
 require_once __DIR__ . '/ParametersProvider.php';
 
 if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
-    WP_CLI::add_command('checksum', 'WPChecksum\Checksum');
+    WP_CLI::add_command('checksum all', array('WPChecksum\Checksum', 'checkAll'));
+    WP_CLI::add_command('checksum plugin', array('WPChecksum\Checksum', 'checkPlugin'));
+    WP_CLI::add_command('checksum theme', array('WPChecksum\Checksum', 'checkTheme'));
+
+    WP_CLI::add_command('checksum quota', array('WPChecksum\Checksum', 'quota'));
+    WP_CLI::add_command('checksum apikey', array('WPChecksum\Checksum', 'apikey'));
+    WP_CLI::add_command('checksum register', array('WPChecksum\Checksum', 'register'));
+
 }
