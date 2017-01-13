38cbc33654824acb919c977de8d422ab0ae28aa9
Erik Torsner <erik@torgesta.com>
2017-01-13 13:52:18 +0100

    Adds diff command

--- wpcli.php
+++ wpcli.php
@@ -6,6 +6,8 @@ if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
     WP_CLI::add_command('checksum plugin', array('WPChecksum\Checksum', 'checkPlugin'));
     WP_CLI::add_command('checksum theme', array('WPChecksum\Checksum', 'checkTheme'));
 
+    WP_CLI::add_command('checksum diff', array('WPChecksum\Checksum', 'diff'));
+
     WP_CLI::add_command('checksum quota', array('WPChecksum\Checksum', 'quota'));
     WP_CLI::add_command('checksum apikey', array('WPChecksum\Checksum', 'apikey'));
     WP_CLI::add_command('checksum register', array('WPChecksum\Checksum', 'register'));
